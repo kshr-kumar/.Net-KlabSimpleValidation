4dcabed6211728061cd76e0535321fa672050754
UI <UI@UI>
2024-08-06 19:50:17 +0530

    Add project files.

---
+++ SimpleValidation/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Razor.Compilation;
+using SimpleValidation.Models;
+
+namespace SimpleValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
+    }
+}
